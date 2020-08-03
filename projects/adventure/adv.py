f399982f34742ea1ab1ee312acd75d288f34d994
Stephanie <sbowl001@fiu.edu>
2020-08-03 12:13:47 +0800

    sprint ok

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,8 @@ from world import World
 import random
 from ast import literal_eval
 
+import collections 
+
 # Load world
 world = World()
 
@@ -24,10 +26,92 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
-
+player.current_room = world.starting_room
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+room_map = {} 
+
+default_direction = 's'
+
+change_direction = {'n': 'e', 'e': 's', 's': 'w', 'w': 'n'}
+direct_inverse_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+def add_room_to_map(room):
+    if room not in room_map:
+        room_map[room] = {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+        exits = room.get_exits()
+        for direction in room_map[room]:
+            if direction not in exits: 
+                room_map[room][direction] = 'NONE'
+        return True
+    else: 
+        return False 
+
+def add_relation_to_map(starting_room, direction, ending_room):
+    room_map[starting_room][direction] = ending_room
+    room_map[ending_room][direct_inverse_direction[direction]] = starting_room
+    # building relationships to the inverse 
+
+def path_to_unexplored_exits(starting_room):
+    visited = set()
+    path_queue = collections.deque()
+
+    path_queue.append((starting_room, []))
+
+    while len(path_queue) > 0:
+        room, path = path_queue.popleft()
+
+        if room in visited:
+            continue 
+        else: 
+            visited.add(room)
+
+        for exit_direction in room_map[room]:
+            if room_map[room][exit_direction] == '?':
+                return path 
+
+            elif room_map[room][exit_direction] != 'NONE':
+                new_path = path.copy()
+                new_path.append(exit_direction)
+                new_room = room_map[room][exit_direction]
+                path_queue.append((new_room, new_path))
+    return [] 
+
+add_room_to_map(player.current_room) 
+
+while len(room_map) < len(room_graph):
+    room = player.current_room
+    exits = room.get_exits()
+
+    unexplored_exit = False 
+
+    direction = default_direction
+
+    for _ in range(len(change_direction)): 
+        if room_map[room][direction] == '?' and direction in exits: 
+            unexplored_exit = True 
+
+            player.travel(direction)
+            next_room = player.current_room
+            traversal_path.append(direction)
+
+            if not add_room_to_map(next_room): 
+                player.travel(direct_inverse_direction[direction])
+                traversal_path.pop()
+            # if visited, go back , take off path to move elsewhere 
+        
+            add_relation_to_map(room, direction, next_room)
+            break
+        else: 
+            direction = change_direction[direction]
+    if not unexplored_exit: 
+        path = path_to_unexplored_exits(room)
+
+        traversal_path.extend(path) 
+        for direction in path: 
+            player.travel(direction)
+
 
 
 
--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -94,6 +94,68 @@ def build_graph(ancestors):
 
     return g 
 
+def dft_recursive(vertex, g):
+    node_id, distance = vertex
+    parents = g.get_neighbors(vertex)
+
+    if len(parents) == 0:
+        return vertex 
+
+    ancient_one = vertex 
+
+    for parent in parents:
+        parent_node = (parent, distance + 1)
+        pair = dft_recursive(parent_node, g)
+
+        if pair[1] > ancient_one[1]:
+            ancient_one = pair
+        elif pair[1] == ancient_one[1] and pair[0] < ancient_one[0]:
+            ancient_one = pair 
+
+    return ancient_one
+
+def recursive_earliers_ancestor(ancestors, starting_node):
+    g = build_graph(ancestors)
+    node = (starting_node, 0)
+
+    ancient_one = dft_recursive(node, g)
+
+    if ancient_one[0] == starting_node:
+        return -1 
+    else: 
+        return ancient_one[0]
+   
+def dft_recursive_2(path, g):
+    current_node = path[-1]
+
+    parents = g.get_neighbors(current_node)
+
+    if len(parents) == 0:
+        return path 
+
+    for parent in parents: 
+        path_copy = list(path)
+        path_copy.append(parent)
+
+        result = dft_recursive_2(path_copy, g)
+
+        if len(result) > len(path):
+            longest_path = result 
+        
+        elif len(reuslt) == len(longest_path):
+            if result[-1] < longest_path[-1]:
+                longest_path = result 
+
+    return longest_path
+
+def earliest_ancestor2(ancestors, starting_node):
+    g = build_graph(ancestors)
+    path = [starting_node]
+    longest_path = dft_recursive_2(path, g)
+    ancient_node = longest_path[-1]
+
+    return ancient_node
+    
 def earliest_ancestor(ancestors, starting_node):
     g = build_graph(ancestors)
 
